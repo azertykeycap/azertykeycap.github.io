632497d85b4630e7e1664231e5d9e7dcdce4efd5
Théo Senoussaoui <theosen95@gmail.com>
2023-01-24 22:44:22 +0100

    feat: starting ui, reworking navigation and adding formatters

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,5 +1,5 @@
-import contentful, { Asset, Entry } from "contentful";
-import type { Document } from "@contentful/rich-text-types";
+import contentful, { Asset, Entry } from 'contentful';
+import type { Document } from '@contentful/rich-text-types';
 export interface TopWarningContentfulInterface {
   richWarning: Document;
 }
@@ -9,6 +9,7 @@ export interface TopWarningInterface {}
 export interface ProfileContentfulInterface {
   readonly title: string;
   readonly slug: string;
+  readonly abbreviation: string;
 }
 
 export interface KeycapArticleContentfulInterface {
@@ -27,7 +28,7 @@ export interface KeycapArticleContentfulInterface {
 
 export type KeycapArticleType = Omit<
   KeycapArticleContentfulInterface,
-  "img" | "profile" | "startDate" | "endDate"
+  'img' | 'profile' | 'startDate' | 'endDate'
 > & {
   img: string;
   profile: string;
@@ -41,5 +42,5 @@ export const contentfulClient = contentful.createClient({
   accessToken: import.meta.env.DEV
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com'
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      colors: {
+        'warm-gray': colors.stone
       },
-    },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
