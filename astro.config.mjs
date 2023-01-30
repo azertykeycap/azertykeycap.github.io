137cbfef3167e4aa283f8fc2ec743b82dee5c19a
Théo Senoussaoui <theosen95@gmail.com>
2023-01-30 14:31:59 +0100

    feat: adding astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()]
+  integrations: [image(), tailwind(), compress()]
 });
