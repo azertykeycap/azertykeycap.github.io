5d32ffe6604aa970d9293317e160d47655d6e3c9
Théo Senoussaoui <theosen95@gmail.com>
2023-02-26 15:32:42 +0100

    feat: trying out vercel server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,58 +1,15 @@
 import { defineConfig } from 'astro/config';
+// https://astro.build/config
+
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
 import compress from 'astro-compress';
-
-// https://astro.build/config
 import vanillaExtract from 'astro-vanilla-extract';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
-import satori from 'astro-satori';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    preact(),
-    image(),
-    vanillaExtract(),
-    compress(),
-    satori({
-      satoriOptionsFactory: async () => {
-        const fontFileRegular = await fetch(
-          'https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf'
-        );
-        const fontRegular = await fontFileRegular.arrayBuffer();
-
-        const fontFileBold = await fetch(
-          'https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf'
-        );
-
-        const fontBold = await fontFileBold.arrayBuffer();
-
-        const options = {
-          width: 1200,
-          height: 630,
-          embedFont: true,
-          fonts: [
-            {
-              name: 'IBM Plex Mono',
-              data: fontRegular,
-              weight: 400,
-              style: 'normal'
-            },
-            {
-              name: 'IBM Plex Mono',
-              data: fontBold,
-              weight: 600,
-              style: 'normal'
-            }
-          ]
-        };
-
-        return options;
-      }
-    })
-  ]
+  integrations: [tailwind(), preact(), image(), vanillaExtract(), compress()],
+  output: 'server',
+  adapter: vercel({ analytics: true })
 });
