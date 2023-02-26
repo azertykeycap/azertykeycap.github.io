6340cef7daf2a6d9488051283b4b04c4ace9afef
Théo Senoussaoui <theosen95@gmail.com>
2023-02-27 00:10:59 +0100

    test: adding background color for vercel test

--- src/components/core/OgImage/OgImage.tsx
+++ src/components/core/OgImage/OgImage.tsx
@@ -9,7 +9,8 @@ export const OgImage = ({ title }: OgImageProps) => (
       justifyContent: 'center',
       alignItems: 'center',
       width: '100%',
-      height: '100%'
+      height: '100%',
+      backgroundColor: 'white'
     }}
   >
     <h1
