623154e4929b6b23b20d2a13211625b55302352e
tsenoussaoui <tsenoussaoui@wedigital.garden>
2023-03-09 20:57:34 +0100

    fix: adding back inter font for testing

--- src/styles/reset.css.ts
+++ src/styles/reset.css.ts
@@ -1,32 +1,32 @@
-import { globalStyle, globalFontFace } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
-globalFontFace('Inter', {
-  fontStyle: 'normal',
-  fontWeight: 400,
-  src: "url(/fonts/Inter-Regular.woff2) format('woff2'), url(/fonts/Inter-Regular.woff) format('woff')",
-  fontDisplay: 'swap'
-});
+// globalFontFace('Inter', {
+//   fontStyle: 'normal',
+//   fontWeight: 400,
+//   src: "url(/fonts/Inter-Regular.woff2) format('woff2'), url(/fonts/Inter-Regular.woff) format('woff')",
+//   fontDisplay: 'swap'
+// });
 
-globalFontFace('Inter', {
-  fontStyle: 'medium',
-  fontWeight: 500,
-  src: "url(/fonts/Inter-Medium.woff2) format('woff2'), url(/fonts/Inter-Medium.woff) format('woff')",
-  fontDisplay: 'swap'
-});
+// globalFontFace('Inter', {
+//   fontStyle: 'medium',
+//   fontWeight: 500,
+//   src: "url(/fonts/Inter-Medium.woff2) format('woff2'), url(/fonts/Inter-Medium.woff) format('woff')",
+//   fontDisplay: 'swap'
+// });
 
-globalFontFace('Inter', {
-  fontStyle: 'semibold',
-  fontWeight: 600,
-  src: "url(/fonts/Inter-SemiBold.woff2) format('woff2'), url(/fonts/Inter-SemiBold.woff) format('woff')",
-  fontDisplay: 'swap'
-});
+// globalFontFace('Inter', {
+//   fontStyle: 'semibold',
+//   fontWeight: 600,
+//   src: "url(/fonts/Inter-SemiBold.woff2) format('woff2'), url(/fonts/Inter-SemiBold.woff) format('woff')",
+//   fontDisplay: 'swap'
+// });
 
-globalFontFace('Inter', {
-  fontStyle: 'bold',
-  fontWeight: 700,
-  src: "url(/fonts/Inter-Bold.woff2) format('woff2'), url(/fonts/Inter-Bold.woff) format('woff')",
-  fontDisplay: 'swap'
-});
+// globalFontFace('Inter', {
+//   fontStyle: 'bold',
+//   fontWeight: 700,
+//   src: "url(/fonts/Inter-Bold.woff2) format('woff2'), url(/fonts/Inter-Bold.woff) format('woff')",
+//   fontDisplay: 'swap'
+// });
 
 globalStyle('*', {
   boxSizing: 'border-box',
