4e89f1194768710198a461a2276db68507489a9a
Théo Senoussaoui <theosen95@gmail.com>
2023-06-25 03:32:36 +0200

    chore: readding vercel adapter for server site build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import compress from 'astro-compress';
 import vanillaExtract from 'astro-vanilla-extract';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vanillaExtract(), compress(), sitemap(), preact()],
   output: 'server',
-  site: 'https://azertykeycaps.fr'
+  site: 'https://azertykeycaps.fr',
+  adapter: vercel()
 });
