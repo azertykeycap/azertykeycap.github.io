150440ee3696a3cc3f4d24743259adb5b385f941
tsenoussaoui <tsenoussaoui@wedigital.garden>
2023-07-14 13:45:54 +0200

    feat: shuffling articles on display

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -28,3 +28,16 @@ export function parseStringWithNewlines(input: string): JSX.Element {
     </>
   );
 }
+
+export function shuffleArray<T>(array: T[]): T[] {
+  const shuffledArray = [...array]; // Create a copy of the original array
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // Generate a random index
+
+    // Swap elements at index i and j
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+}
