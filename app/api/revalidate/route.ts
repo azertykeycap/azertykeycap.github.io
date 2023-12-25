e024830240e0bd4905d40d45f1725f785ad88ccb
Théo Senoussaoui <theosen95@gmail.com>
2023-12-26 00:43:58 +0100

    chore: adding contentful secret

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,6 +2,16 @@ import { revalidatePath } from "next/cache";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  revalidatePath("/profil/[slug]", "layout");
-  return Response.json({ revalidated: true, now: Date.now() });
+  const secret = request.nextUrl.searchParams.get("secret");
+
+  if (secret && secret === process.env.CONTENTFUL_WEBHOOK_SECRET) {
+    revalidatePath("/profil/[slug]", "layout");
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: "Missing secret in webhook",
+  });
 }
